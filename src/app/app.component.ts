dcd67d8f0103c276db2c6c5459b99ba195da1990
Franco Ojeda <fojedavideos@gmail.com>
2023-01-27 09:25:55 -0500

    implementacion de login con validacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ejercicio-28';
+  
+  formLogin!:FormGroup;
+
+  msgCorreoInvalido = false;
+  msgCorreoValido = false;
+
+  constructor( private router:Router, private formBuilder: FormBuilder ){
+    this.formLogin = new FormGroup({
+      user: new FormControl(),
+      password: new FormControl(),
+      termino: new FormControl()
+    })
+  }
+
+  ngOnInit(): void {
+    this.formLogin = this.formBuilder.group({
+      user:['', 
+        [
+          Validators.required,
+          Validators.minLength(5)
+        ]
+      ],
+      password:['', 
+        [
+          Validators.required,
+          Validators.minLength(4)
+        ]
+      ],
+      termino:['', 
+        [
+          Validators.requiredTrue,
+        ]
+      ]
+    });
+  }
+
+  onSubmit(){
+    this.msgCorreoValido = true;
+    setTimeout(() => {
+      this.msgCorreoValido = false;
+    }, 6000);
+  }
 }
